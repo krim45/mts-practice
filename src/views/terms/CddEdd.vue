4371971d77143cb32bcec65b096bd10f94a64f83
krim45 <krim45@naver.com>
2022-04-28 23:33:38 +0900

    :feat: CddEd, ServiceTerms

--- src/views/terms/CddEdd.vue
+++ src/views/terms/CddEdd.vue
@@ -1,9 +1,77 @@
 <template>
-  <div></div>
+  <TheContainer :option="headerOption">
+    <form @submit="onSubmit" action="http://localhost:8080/terms/research">
+      <ul>
+        <li v-for="(item, idx) in list" :key="idx">
+          <span>{{ item.label }}</span>
+          <select @change="(e) => select(e, idx)" :name="item.name">
+            <option
+              v-for="(value, index) in item.options"
+              :key="index"
+              :value="value"
+            >
+              {{ value }}
+            </option>
+          </select>
+        </li>
+      </ul>
+      <FixedButton>다음</FixedButton>
+    </form>
+  </TheContainer>
 </template>
 
 <script>
-export default {};
+import FixedButton from "../../components/FixedButton.vue";
+import TheContainer from "../../layouts/TheContainer.vue";
+export default {
+  components: { TheContainer, FixedButton },
+  data() {
+    return {
+      headerOption: {
+        title: "고객정보 확인",
+        content: "닫기",
+      },
+      valueArr: [null, null, null, null],
+      list: [
+        {
+          label: "직업",
+          name: "job",
+          options: ["직업선택", "1", "2"],
+        },
+        {
+          label: "자금출처",
+          name: "money",
+          options: ["자금출처 선택", "1", "2"],
+        },
+        {
+          label: "투자목적",
+          name: "proposal",
+          options: ["투자목적 선택", "1", "2"],
+        },
+        {
+          label: "우편번호",
+          name: "zipCode",
+          options: ["우편번호 선택", "1", "2"],
+        },
+      ],
+    };
+  },
+  computed: {
+    isAllSelected() {
+      console.log(this.valueArr);
+      return this.valueArr.every((v) => v !== null);
+    },
+  },
+  methods: {
+    select(e, idx) {
+      this.valueArr[idx] = e.target.value;
+    },
+    onSubmit(e) {
+      console.log(e);
+      // this.$router.push("research");
+    },
+  },
+};
 </script>
 
 <style>
--- src/views/terms/ServiceTerms.vue
+++ src/views/terms/ServiceTerms.vue
@@ -1,10 +1,93 @@
 <template>
-  <div></div>
+  <TheContainer :option="haederOption">
+    <form @submit="onSubmit" action="http://localhost:8080/terms/cdd-edd">
+      <div>
+        <p>
+          <label>
+            <input type="checkbox" @change="checkAll" />
+            <span>모두 동의합니다.</span>
+          </label>
+        </p>
+      </div>
+      <ul>
+        <li v-for="(term, idx) in termsList" :key="idx">
+          <label>
+            <input
+              type="checkbox"
+              @click="() => checkTerms(idx)"
+              :checked="term.checked"
+              :name="term.content"
+            />
+            <span>{{ term.content }}</span>
+          </label>
+          <span>
+            <a :href="term.href" target="_black">
+              <img src="@/assets/icon-go.svg" alt="약관 디테일" />
+            </a>
+          </span>
+        </li>
+      </ul>
+      <FixedButton>다음</FixedButton>
+    </form>
+  </TheContainer>
 </template>
 
 <script>
-export default {};
+import FixedButton from "../../components/FixedButton.vue";
+import TheContainer from "../../layouts/TheContainer.vue";
+export default {
+  components: { TheContainer, FixedButton },
+  data() {
+    return {
+      haederOption: {
+        title: "서비스신청 및 약관동의",
+        content: "닫기",
+      },
+      allChecked: false,
+      termsList: [
+        {
+          checked: false,
+          content: "위험 고지",
+          required: true,
+          href: "https://www.naver.com/",
+        },
+        {
+          checked: false,
+          content: "이용약관",
+          required: true,
+          href: "https://www.naver.com/",
+        },
+        {
+          checked: false,
+          content: "제공 동의",
+          required: true,
+          href: "https://www.naver.com/",
+        },
+        {
+          checked: false,
+          content: "선택 제공 동의",
+          required: false,
+          href: "https://www.naver.com/",
+        },
+      ],
+    };
+  },
+  methods: {
+    checkAll() {
+      this.allChecked = !this.allChecked;
+      this.termsList.forEach((term) => {
+        term.checked = this.allChecked;
+      });
+    },
+    checkTerms(idx) {
+      this.termsList[idx].checked = !this.termsList[idx].checked;
+    },
+    onSubmit() {
+      // this.$router.push("cdd-edd");
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
 </style>
